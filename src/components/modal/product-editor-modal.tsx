feb8efe65c231968fff87afbb1d4271586c07330
gitdevjin <dev.hjshin@gmail.com>
2026-09-19 15:50:48 -0400

    product categories added

--- src/components/modal/product-editor-modal.tsx
+++ src/components/modal/product-editor-modal.tsx
@@ -18,7 +18,15 @@ import {
   SelectValue,
 } from "../ui/select";
 
-const CATEGORIES = ["Mask", "Serum", "Toner", "Cleanser", "Others"] as const;
+const CATEGORIES = [
+  "Mask",
+  "Serum",
+  "Toner",
+  "Lotion",
+  "Cream",
+  "Cleanser",
+  "Others",
+] as const;
 
 export default function ProductEditorModal() {
   const store = useProductEditorModalStore();
